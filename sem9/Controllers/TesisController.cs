6c8ec5ece1d2b99fd2891fea51a754703711fbc0
laszlo-caballero <laszlocaballero@gmail.com>
2025-05-24 16:43:32 -0500

    Agregar controlador Tesis y vista de índice

---
+++ sem9/Controllers/TesisController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sem9.Controllers
+{
+    public class TesisController : Controller
+    {
+        // GET: TesisController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
